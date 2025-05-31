39a61b3f775c79ad441934351d9d8a0a6b44af2f
leodipaula <leofernandes9@gnail.com>
2025-05-30 23:31:17 -0300

    feat: perfil do usuario

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,44 @@
 import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { type ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                        Blog Pessoal Generation | Copyright: {data}
+                        Blog Pessoal | Copyright: {data}
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/leonardo-fernandes-pf/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+    return (
+        <>
+            {component}
         </>
     )
 }
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,23 +1,26 @@
-import { useContext } from "react";
+import { type ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
-
-
 
 function Navbar() {
 
     const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
+
         handleLogout()
         alert('O Usuário foi desconectado com sucesso!')
         navigate('/')
     }
 
-    return (
-        <>
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
 
@@ -28,11 +31,19 @@ function Navbar() {
                         <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
+
+        )
+
+    }
+
+    return (
+        <>
+            {component}
         </>
     )
 }
